88cd3af5f68aaa9e07c113efac79a0e78900239e
Vaishnavi <vaishnavi.bhaarde@blimp.co.in>
2019-06-10 17:16:30 +0530

    example to create new components in react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard >
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 1pm"
+          content="1st comment"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 2pm" content="2nd comment" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Today at 6pm"content="3rd comment" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+    </div>
+  );
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
